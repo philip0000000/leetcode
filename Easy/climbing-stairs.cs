20964b7a5b25f951fda4feab77101094863b6159
philip0000000 <92891603+philip0000000@users.noreply.github.com>
2022-11-18 14:07:46 +0100

    Create climbing-stairs.cs

---
+++ Easy/climbing-stairs.cs
@@ -0,0 +1,19 @@
+//Fibonacci number
+public class Solution {
+    public int ClimbStairs(int n) {
+        int n0 = 0;
+        int n1 = 1;
+        for (int i = 0; i < n; i++)
+        {
+            if (i % 2 == 1)
+                n1 = n0 + n1;
+            else
+                n0 = n0 + n1;
+        }
+
+        if (n0 > n1)
+            return n0;
+        return n1;
+    }
+}
+
