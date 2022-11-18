97a15b513418c7c984d512a84ca4a9f92c05feca
philip0000000 <92891603+philip0000000@users.noreply.github.com>
2022-11-18 16:53:06 +0100

    Update LongestPassword.cs

--- codility/LongestPassword.cs
+++ codility/LongestPassword.cs
@@ -18,6 +18,52 @@ class Solution {
     public int solution(string S) {
         // write your code in C# 6.0 with .NET 4.7 (Mono 6.12)
         int ret = -1; // default value is -1using System;
+        int NumLetters = 0;
+        int NumDigits = 0;
+        bool InvalidPassword = false;
+
+        foreach (char c in S)
+        {
+            if (c == ' ') // new password
+            {
+                // 1st calculat if it is bigger BinaryGap.cs
+                if (InvalidPassword == false && // contain only alphanumerical characters
+                    NumLetters % 2 == 0 &&      // even number of letters
+                    NumDigits % 2 != 0)         // odd number of digits
+                    if ((NumLetters + NumDigits) > ret)
+                        ret = (NumLetters + NumDigits);
+
+                // 2nd reset values
+                NumLetters = 0;
+                NumDigits = 0;
+                InvalidPassword = false;
+            }
+            else
+            {
+                if (InvalidPassword == false)
+                {
+                    if (IsLetter(c) == true)
+                        NumLetters++;
+                    else if (IsDigit(c) == true)
+                        NumDigits++;
+                    else // invalid character
+                        InvalidPassword = true;
+                }
+            }
+        }
+
+        // check again
+        if (InvalidPassword == false && // contain only alphanumerical characters
+            NumLetters % 2 == 0 &&      // even number of letters
+            NumDigits % 2 != 0)        // odd number of digits
+            if ((NumLetters + NumDigits) > ret)
+                ret = (NumLetters + NumDigits);
+
+        return ret;
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////
 // you can also use other imports, for example:
 // using System.Collections.Generic;
 
@@ -75,51 +121,6 @@ class Solution {
         if (InvalidPassword == false && // contain only alphanumerical characters
             NumLetters % 2 == 0 &&      // even number of letters
             NumDigits % 2 != 0)         // odd number of digits
-            if ((NumLetters + NumDigits) > ret)
-                ret = (NumLetters + NumDigits);
-
-        return ret;
-    }
-}
-
-        int NumLetters = 0;
-        int NumDigits = 0;
-        bool InvalidPassword = false;
-
-        foreach (char c in S)
-        {
-            if (c == ' ') // new password
-            {
-                // 1st calculat if it is bigger
-                if (InvalidPassword == false && // contain only alphanumerical characters
-                    NumLetters % 2 == 0 &&      // even number of letters
-                    NumDigits % 2 != 0)         // odd number of digits
-                    if ((NumLetters + NumDigits) > ret)
-                        ret = (NumLetters + NumDigits);
-
-                // 2nd reset values
-                NumLetters = 0;
-                NumDigits = 0;
-                InvalidPassword = false;
-            }
-            else
-            {
-                if (InvalidPassword == false)
-                {
-                    if (IsLetter(c) == true)
-                        NumLetters++;
-                    else if (IsDigit(c) == true)
-                        NumDigits++;
-                    else // invalid character
-                        InvalidPassword = true;
-                }
-            }
-        }
-
-        // check again
-        if (InvalidPassword == false && // contain only alphanumerical characters
-            NumLetters % 2 == 0 &&      // even number of letters
-            NumDigits % 2 != 0)        // odd number of digits
             if ((NumLetters + NumDigits) > ret)
                 ret = (NumLetters + NumDigits);
 
