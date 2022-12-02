194e82d65804700a39c5fb716bbf887e876b51ec
philip0000000 <92891603+philip0000000@users.noreply.github.com>
2022-12-02 06:17:36 +0100

    Update binary-search.cs

--- Easy/binary-search.cs
+++ Easy/binary-search.cs
@@ -2,7 +2,7 @@ public class Solution {
     public int BinarySearch(int[] nums, int l, int r, int target)
     {
         if (l > r) return -1;
-        int m = l + (r-l)/2;
+        int m = (l+r)/2;
         return (nums[m] == target) ? m :
                (nums[m] < target)  ? BinarySearch(nums, m + 1, r, target) : BinarySearch(nums, l, m - 1, target);
     }
