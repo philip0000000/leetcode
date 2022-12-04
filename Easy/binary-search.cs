7f2b82f4b4e720c0937a02eea3d474159d1d1aa9
philip0000000 <92891603+philip0000000@users.noreply.github.com>
2022-12-04 02:28:38 +0100

    Update binary-search.cs

--- Easy/binary-search.cs
+++ Easy/binary-search.cs
@@ -1,3 +1,4 @@
+// 1st recursion
 public class Solution {
     public int BinarySearch(int[] nums, int l, int r, int target)
     {
@@ -8,5 +9,21 @@ public class Solution {
     }
     public int Search(int[] nums, int target) {
         return BinarySearch(nums, 0, nums.Length - 1, target);
+    }
+}
+
+// 2nd imperative
+public class Solution {
+    public int Search(int[] nums, int target) {
+        int l = 0, r = nums.Length, max = nums.Length;
+        while (l <= r)
+        {
+            int m = (l+r)/2;
+            if (m >= max) return -1;
+            if (nums[m] == target) return m;
+            if (nums[m] < target) l = m + 1;
+            else r = m - 1;
+        }
+        return -1;
     }
 }
