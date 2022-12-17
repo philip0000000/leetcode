475b24847d1d797688b050e0379201eee9fa4213
philip0000000 <92891603+philip0000000@users.noreply.github.com>
2022-12-17 04:40:33 +0100

    Create check-if-n-and-its-double-exist.cs

---
+++ Easy/check-if-n-and-its-double-exist.cs
@@ -0,0 +1,31 @@
+// 1st
+for (int i = 0; i < arr.Length; i++)
+    for (int j = 0; j < arr.Length; j++)
+        if (i != j && arr[i] == arr[j]*2)
+            return true;
+
+return false;
+
+// 2nd
+var num = new List<int>();
+
+for (int i = 0; i < arr.Length; i++)
+    num.Add(arr[i] * 2);
+for (int i = 0; i < arr.Length; i++)
+    if (num.Contains(arr[i]))
+        if (i != num.IndexOf(arr[i]))
+            return true;
+
+return false;
+
+// 3rd
+HashSet<int> seen = new HashSet<int>();
+foreach (int x in arr)
+{
+    if (seen.Contains(x * 2) || (x % 2 == 0 && seen.Contains(x / 2)))
+    {
+        return true;
+    }
+    seen.Add(x);
+}
+return false;
