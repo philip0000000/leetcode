47f1e5c3eea22050c2613d291fda210791e101c1
philip0000000 <92891603+philip0000000@users.noreply.github.com>
2022-12-25 17:50:27 +0100

    Update roman-to-integer.cs

--- Easy/roman-to-integer.cs
+++ Easy/roman-to-integer.cs
@@ -1,14 +1,14 @@
 public class Solution {
     static readonly Dictionary<char, int> RomanNrDict = new Dictionary<char, int>()
-        {
-            { 'I', 1 },
-            { 'V', 5 },
-            { 'X', 10 },
-            { 'L', 50 },
-            { 'C', 100 },
-            { 'D', 500 },
-            { 'M', 1000 }
-        };
+    {
+        { 'I', 1 },
+        { 'V', 5 },
+        { 'X', 10 },
+        { 'L', 50 },
+        { 'C', 100 },
+        { 'D', 500 },
+        { 'M', 1000 }
+    };
     public int RomanToInt(string s) {
         int num = RomanNrDict[s[0]];
         int oldNumber = RomanNrDict[s[0]];
