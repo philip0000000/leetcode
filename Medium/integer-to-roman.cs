c5b394147b65ae7cf375b0380f0d6e971b70f283
philip0000000 <92891603+philip0000000@users.noreply.github.com>
2022-12-25 17:50:05 +0100

    Update integer-to-roman.cs

--- Medium/integer-to-roman.cs
+++ Medium/integer-to-roman.cs
@@ -74,21 +74,21 @@ public class Solution {
 // 2nd
 public class Solution {
     static readonly Dictionary<int, string> RomanNrDict = new Dictionary<int, string>()
-        {
-            {1000, "M"},
-            {900, "CM"},
-            {500, "D"},
-            {400, "CD"},
-            {100, "C"},
-            {90, "XC"},
-            {50, "L"},
-            {40, "XL"},
-            {10, "X"},
-            {9, "IX"},
-            {5, "V"},
-            {4, "IV"},
-            {1, "I"}
-        };
+    {
+        {1000, "M"},
+        {900, "CM"},
+        {500, "D"},
+        {400, "CD"},
+        {100, "C"},
+        {90, "XC"},
+        {50, "L"},
+        {40, "XL"},
+        {10, "X"},
+        {9, "IX"},
+        {5, "V"},
+        {4, "IV"},
+        {1, "I"}
+    };
     public string IntToRoman(int num) {
         string ret = "";
         foreach(KeyValuePair<int, string> entry in RomanNrDict)
