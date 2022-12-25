2cbc6e6bcf5a9c7e3bca4d37a0f7572e33464277
philip0000000 <92891603+philip0000000@users.noreply.github.com>
2022-12-25 17:40:29 +0100

    Update integer-to-roman.cs

--- Medium/integer-to-roman.cs
+++ Medium/integer-to-roman.cs
@@ -75,13 +75,13 @@ public class Solution {
 public class Solution {
     static readonly Dictionary<int, string> RomanNrDict = new Dictionary<int, string>()
         {
-            { 1000, "M"},
-            { 900, "CM"},
-            { 500, "D"},
-            { 400, "CD"},
-            { 100, "C"},
-            { 90, "XC"},
-            { 50, "L"},
+            {1000, "M"},
+            {900, "CM"},
+            {500, "D"},
+            {400, "CD"},
+            {100, "C"},
+            {90, "XC"},
+            {50, "L"},
             {40, "XL"},
             {10, "X"},
             {9, "IX"},
