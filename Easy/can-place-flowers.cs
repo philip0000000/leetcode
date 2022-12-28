06828c7208e72ac558af64c2dc7af19032c05f01
philip0000000 <92891603+philip0000000@users.noreply.github.com>
2022-12-28 04:30:11 +0100

    Create can-place-flowers.cs

---
+++ Easy/can-place-flowers.cs
@@ -0,0 +1,46 @@
+// 1st
+public class Solution {
+    public bool CanPlaceFlowers(int[] flowerbed, int n) {
+        if (flowerbed.Length > 1)
+        {
+            if (flowerbed[0] == 0 && flowerbed[1] == 0)
+            {
+                flowerbed[0] = 1;
+                n--;
+            }
+            if (flowerbed[flowerbed.Length - 1] == 0 && flowerbed[flowerbed.Length - 2] == 0)
+            {
+                flowerbed[flowerbed.Length - 1] = 1;
+                n--;
+            }
+            for (int i = 1; i+1 < flowerbed.Length - 1; i++)
+                if (flowerbed[i-1] == 0 &&
+                    flowerbed[i] == 0 &&
+                    flowerbed[i+1] == 0)
+                {
+                    flowerbed[i] = 1;
+                    n--;
+                    if (n < 1)
+                        break;
+                }
+        }
+        else if (flowerbed[0] == 0)
+            n--;
+        return n > 0 ? false : true;
+    }
+}
+
+// 2nd 50%~
+public class Solution {
+    public bool CanPlaceFlowers(int[] flowerbed, int n) {
+        for (int i = 0; i < flowerbed.Length; i++)
+            if (flowerbed[i] == 0 && 
+                (i == 0 || flowerbed[i - 1] == 0) && 
+                (i == flowerbed.Length - 1 || flowerbed[i + 1] == 0))
+            {
+                flowerbed[i] = 1;
+                n--;
+            }
+        return n <= 0;
+    }
+}
