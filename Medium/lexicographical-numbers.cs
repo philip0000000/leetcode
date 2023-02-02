0cf246d328522ed71f14b780b09fa79a0a49d53c
philip0000000 <92891603+philip0000000@users.noreply.github.com>
2023-02-02 15:03:24 +0100

    Create lexicographical-numbers.cs

---
+++ Medium/lexicographical-numbers.cs
@@ -0,0 +1,25 @@
+//https://leetcode.com/problems/lexicographical-numbers/submissions/890071576/
+public class Solution {
+    public IList<int> LexicalOrder(int n) {
+        var arr = new int[n];
+        arr[0] = 1;
+        for (int i = 1, num = 1; i < n; i++)
+        {
+            if (num * 10 <= n)
+            {
+                num *= 10;
+                arr[i] = num;
+            }
+            else
+            {
+                if (num >= n)
+                    num /= 10;
+                while ((num % 10) == 9)
+                    num /= 10;
+                num++;
+                arr[i] = num;
+            }
+        }
+        return arr;
+    }
+}
