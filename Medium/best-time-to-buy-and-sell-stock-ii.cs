b1fb9f64ce528888ce0962242826684fcba7b76f
philip0000000 <92891603+philip0000000@users.noreply.github.com>
2023-02-09 10:13:56 +0100

    Update best-time-to-buy-and-sell-stock-ii.cs

--- Medium/best-time-to-buy-and-sell-stock-ii.cs
+++ Medium/best-time-to-buy-and-sell-stock-ii.cs
@@ -11,3 +11,14 @@ public class Solution {
         return ret;
     }
 }
+
+// 2nd
+public class Solution {
+    public int MaxProfit(int[] prices) {
+        int ret = 0;
+        for (int i = prices.Length-1; i > 0; i--)
+            if (prices[i-1] < prices[i])
+                ret += prices[i] - prices[i-1];
+        return ret;
+    }
+}
