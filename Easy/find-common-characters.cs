d8dcfa8c78e8e88bded2f569f9539ee271f19702
philip0000000 <92891603+philip0000000@users.noreply.github.com>
2023-02-17 23:13:08 +0100

    Update find-common-characters.cs

--- Easy/find-common-characters.cs
+++ Easy/find-common-characters.cs
@@ -1,3 +1,4 @@
+// 1st
 public class Solution {
     public IList<string> CommonChars(string[] words) {
         var dict = new Dictionary<char, int>();
@@ -29,5 +30,27 @@ public class Solution {
             for (int i = 0; i < kvp.Value; i++)
                 list.Add(kvp.Key.ToString());
         return list;
+    }
+}
+
+// 2nd https://leetcode.com/problems/find-common-characters/submissions/900001780/
+public class Solution {
+    public IList<string> CommonChars(string[] words) {
+        var arr0 = new int[26];
+        for (int j = 0; j < words[0].Length; j++)
+            arr0[words[0][j] - 'a']++;
+        for (int i = 1; i < words.Length; i++)
+        {
+            var arr1 = new int[26];
+            for (int j = 0; j < words[i].Length; j++)
+                arr1[words[i][j] - 'a']++;
+            for (int n = 0; n < 26; n++)
+                arr0[n] = Math.Min(arr0[n], arr1[n]);
+        }
+        var ret = new List<string>();
+        for (int i = 0; i < 26; i++)
+            for (int j = 0; j < arr0[i]; j++)
+                ret.Add(((char)('a' + i)).ToString());
+        return ret;
     }
 }
