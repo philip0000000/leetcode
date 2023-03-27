0e633d6735b03fc14bdcac8488758ecef6f032f9
philip0000000 <92891603+philip0000000@users.noreply.github.com>
2023-03-27 11:00:41 +0200

    Update maximum-score-after-splitting-a-string.cs

--- Easy/maximum-score-after-splitting-a-string.cs
+++ Easy/maximum-score-after-splitting-a-string.cs
@@ -1,19 +1,19 @@
 public class Solution {
     public int MaxScore(string s) {
         int zeros = s[0] == '0' ? 1 : 0;
-        int one = 0;
+        int ones = 0;
         for (int i = s.Length-1; i > 0; i--)
             if (s[i]=='1')
-                one++;
-        int max = one + zeros;
+                ones++;
+        int max = ones + zeros;
         for (int i = 1; i < s.Length-1; i++)
         {
             if (s[i]=='0')
                 zeros++;
             else
-                one--;
-            if (one + zeros > max)
-                max = one + zeros;
+                ones--;
+            if (ones + zeros > max)
+                max = ones + zeros;
         }
         return max;
     }
