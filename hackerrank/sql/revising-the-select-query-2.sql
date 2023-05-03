eb7f8789c7ffb0eedfe43cb569ba0281b904a5ab
Philip <92891603+philip0000000@users.noreply.github.com>
2023-05-03 23:04:54 +0200

    Create revising-the-select-query-2.sql

---
+++ hackerrank/sql/revising-the-select-query-2.sql
@@ -0,0 +1,7 @@
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+SELECT name
+FROM city
+WHERE population > 120000 AND countrycode = 'USA';
