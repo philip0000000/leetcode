be88205eff565cabfedcbc42bb80a93bd8ce0886
Philip <92891603+philip0000000@users.noreply.github.com>
2023-05-03 20:35:41 +0200

    Delete revising-the-select-query.sql

--- hackerearth/sql/revising-the-select-query.sql
+++
@@ -1,7 +0,0 @@
-/*
-Enter your query here.
-Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
-*/
-SELECT *
-FROM city
-WHERE population > 100000 AND CountryCode = 'USA';
